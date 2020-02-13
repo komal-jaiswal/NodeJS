5c946d653bbba2ea590b155ca6e9a8bb39a47723
komal-jaiswal <jaiswalkomal18@gmail.com>
2020-02-13 22:47:56 +0530

    sending json to client,sending html to client, sending file to client

--- Client-Server-communication/server.js
+++ Client-Server-communication/server.js
@@ -2,15 +2,24 @@ var http=require('http');
 var fs=require('fs');
 
 var myReadStream=fs.createReadStream(__dirname+'/datasendtoserver.txt');
+var mywriteStream=fs.createWriteStream(__dirname+'/writetext.txt');
 myReadStream.on('data',function (chunk) {
   console.log("new chunk received");
   console.log(chunk);
-})
+  mywriteStream.write(chunk);
+});
+//instead of above 4 lines you can use single line as below
+//myReadStream.pipe(mywriteStream);
 
 var server=http.createServer(function (req,res) {
   console.log("reuest made" + req.url);
   res.writeHead(200,{'Content-Type':'text/plain'});
-  res.end("hello komal");
+  //here will send file content to client browser instead of wriiting in files
+  var myReadStream1=fs.createReadStream(__dirname+'/datasendtoserver.txt','utf8');
+console.log(__dirname);
+  myReadStream1.pipe(res);
+
+//  res.end("hello komal");
 });
 
 server.listen(3000,'127.0.0.1');
